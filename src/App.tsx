c9482a0783b4bfc61ba0e84492044e653fadadea
kionys <kiwon7271@naver.com>
2024-08-07 15:55:31 +0900

    add: router 컴포넌트 추가 및 챕터 page 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import Router from "components/router";
 import "./App.css";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>home page</h1>} />
-      <Route path="/posts" element={<h1>posts list page</h1>} />
-      <Route path="/posts/:id" element={<h1>posts detail page</h1>} />
-      <Route path="/posts/new" element={<h1>posts new page</h1>} />
-      <Route path="/posts/edit/:id" element={<h1>posts edit page</h1>} />
-      <Route path="/profile" element={<h1>posts profile page</h1>} />
-      <Route
-        path="/profile/edit/:id"
-        element={<h1>posts profile edit page</h1>}
-      />
-      <Route
-        path="/notifications"
-        element={<h1>posts notifications page</h1>}
-      />
-      <Route path="/search" element={<h1>posts search page</h1>} />
-      <Route path="/login" element={<h1>posts login page</h1>} />
-      <Route path="/signup" element={<h1>posts signup page</h1>} />
-      <Route path="*" element={<Navigate replace to={"/"} />} />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
